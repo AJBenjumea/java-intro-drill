aeceb87a17a1f637c3fb3a5aba36f44c7a50cae5
AJ Benjumea <aj@AJs-Mac-mini.fios-router.home>
2021-04-22 17:15:42 -0400

    Pass all Playlist Tests

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -1,4 +1,47 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Playlist {
+    String name;
+    List<Song> songList = new ArrayList<>();
+    public Playlist(String name) {
+        this.name = name;
+
+    }
+
+    public boolean isEmpty() {
+        return songList.isEmpty();
+    }
+
+    public void addSong(Song song) {
+        songList.add(song);
+    }
+
+    public String[] songNames() {
+        String[] result = new String[songList.size()];
+        for(int i = 0; i < songList.size(); i++) {
+            result[i] = songList.get(i).title;
+        }
+        return result;
+    }
+
+    public int totalDuration() {
+        int result = 0;
+        for(int i = 0; i < songList.size(); i++) {
+            result += songList.get(i).durationInSeconds;
+        }
+        return result;
+    }
+
+    public void swap(Song a, Song b) {
+        int tempA = songList.indexOf(a);
+        int tempB = songList.indexOf(b);
+        songList.set(tempA, b);
+        songList.set(tempB, a);
+    }
+
+    public void removeSong(Song a) {
+        songList.remove(a);
+    }
 }
+
